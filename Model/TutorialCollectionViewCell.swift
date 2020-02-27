5976b1b1bbae7cbee0269900cf2b64e3140d627b
Nathalia <nathaliamelare@gmail.com>
2020-02-27 20:15:02 -0300

    Codigo comentado deletado

--- Model/TutorialCollectionViewCell.swift
+++ Model/TutorialCollectionViewCell.swift
@@ -10,29 +10,8 @@ import UIKit
 
 class TutorialCollectionViewCell: UICollectionViewCell {
     
-    var tip: Receita!
-    
-    // MARK: - Set Up Tutorial Text
     let text = UITextView()
-//        = {
-//        let tipsText = UITextView()
-//        tipsText.text = "1.  Pegue o ovo e segure-o com uma mão em formato de concha. Bata a superfície do ovo, de preferência próxima ao centro, na quina da mesa ou da pia devagar até rachar. \n \n2.  Não use muita força, pois você pode quebrar a gema ou ovo pode estraçalhar.\nSegure o ovo em cima da tigela (ou do local que vai querer coloca-lo). \n \n3.  Depois disso, coloque seus dedões sobre a rachadura e seus outros dedos na parte de baixo do ovo. Agora, devagar, pressione seus dedões na rachadura e puxe os outros dedos ao mesmo tempo, formando um coração invertido. Deixe o ovo cair na tigela e pronto! Já pode adiciona-lo a suas receitas."
-//        tipsText.textColor = #colorLiteral(red: 0.4980472922, green: 0.4950307012, blue: 0.5003677011, alpha: 1)
-//        tipsText.isEditable = false
-//        tipsText.backgroundColor = .none
-//        return tipsText
-//    }()
-    
-    // MARK: - Set Up Tutorial Title
     let title = UILabel()
-//        = {
-//        let title = UILabel()
-//        title.text = "Como Quebrar Ovos"
-//        title.textColor = #colorLiteral(red: 0.9320240021, green: 0.7159546018, blue: 0.3065403104, alpha: 0.8470588235)
-//        title.font = .systemFont(ofSize: 20, weight: UIFont.Weight(rawValue: 0.3))
-//        title.textAlignment = .center
-//        return title
-//    }()
     
     override init(frame: CGRect) {
         super .init(frame: frame)
